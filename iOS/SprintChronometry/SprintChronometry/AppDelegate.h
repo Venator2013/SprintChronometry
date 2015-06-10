981d34672856d05d14c06719656e016dae912dee
Venator <sv_jaeger@gmx.de>
2015-06-10 15:16:06 +0200

    Initial commit for iOS App

---
+++ iOS/SprintChronometry/SprintChronometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SprintChronometry
+//
+//  Created by Sven Jäger on 10/06/15.
+//  Copyright (c) 2015 Sven Jäger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
