f1b930885dcd88d514143800406d3f0d2f08567f
Venator <sv_jaeger@gmx.de>
2015-06-10 15:16:06 +0200

    Initial commit for MFNEt Solution

---
+++ NetMF/SprintChronometry/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using System;
+using Microsoft.SPOT;
+
+namespace SprintChronometry
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			
+		}
+	}
+}
---
+++ iOS/SprintChronometry/SprintChronometry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SprintChronometry
+//
+//  Created by Sven Jäger on 10/06/15.
+//  Copyright (c) 2015 Sven Jäger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
